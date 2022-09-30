dfab5fc3a8cac1399cc10378c4a02fd7e3d62001
big-lip-bob <41026257+big-lip-bob@users.noreply.github.com>
2022-09-30 12:02:24 +0200

    I am, I am, I am, I am Unstoppable

--- 3/5/devinette.py
+++ 3/5/devinette.py
@@ -3,12 +3,13 @@ import random
 hidden = random.randint(0, 100)
 max = 6
 tries = 0
-while tries != max:
+while True:
     guess = int(input())
     tries += 1
     if hidden == guess:
         print(f"Gagné en {tries} essai(s) !")
         break
-    if max != tries: print("Trop grand" if hidden < guess else "Trop petit")
-else:
-    print(f"Perdu ! Le secret était {hidden}")
+    if tries < max: print("Trop grand" if hidden < guess else "Trop petit")
+    else:
+        print(f"Perdu ! Le secret était {hidden}")
+        break
