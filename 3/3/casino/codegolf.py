a02f5e86cdbea8cbd975f5efb9755d441a5da753
big-lip-bob <41026257+big-lip-bob@users.noreply.github.com>
2022-10-01 20:49:01 +0200

    <3 BF

--- 3/3/casino/codegolf.py
+++ 3/3/casino/codegolf.py
@@ -1,3 +1,2 @@
-P=int(input())
-T=int(input())
+P=int(input());T=int(input())
 print((((T!=0)&((T<11)^T^P),(T^P)&1)[P<15]*20,(T==P)*120)[P<13])
--- 3/5/somme_f/une_boucle.py
+++ 3/5/somme_f/une_boucle.py
@@ -1,11 +1,7 @@
 count = int(input())
-count = count if count >= 0 else 1 << 40
 
 sum = 0
-for _ in range(0, count):
-    num = input()
-    if num == 'F': break
+for _ in range(0, count if count >= 0 else 1 << 40) and (num := input()) != 'F':
     sum += int(num)
-    count -= 1
 
 print(sum)
