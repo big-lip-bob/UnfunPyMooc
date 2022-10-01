8680336b0c552c1e1c0653240bcf014bd91c247b
big-lip-bob <41026257+big-lip-bob@users.noreply.github.com>
2022-10-01 19:11:52 +0200

    I am, I am, I am, I am Unstoppable

--- 5/2/prime_numbers.py
+++ 5/2/prime_numbers.py
@@ -1,11 +1,17 @@
 def prime_numbers(n):
     if type(n) != int or n < 0: return None
-    primes = [2, 3, 5, 7, 11, 13]
+    primes = [2, 3] # 3, 5, 7, 11, 13]
     if n < len(primes): return primes[:n]
-    maybe = 17
+
+    maybe = 5
+    sqrt = maybe ** 0.5
+
     while len(primes) < n:
-        for div in primes:
-            if maybe % div == 0: break
+        idx = 1
+        while primes[idx] <= sqrt:
+            if maybe % primes[idx] == 0: break
+            idx += 1
         else: primes.append(maybe)
         maybe += 2
+        sqrt = maybe ** 0.5
     return primes
