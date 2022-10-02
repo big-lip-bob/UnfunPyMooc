64251fe084f6838f13c84483b691e965b564696a
big-lip-bob <41026257+big-lip-bob@users.noreply.github.com>
2022-10-02 20:42:15 +0200

    := yikes

--- 3/5/sin_aprox.py
+++ 3/5/sin_aprox.py
@@ -6,12 +6,8 @@ lastFact = 1 * 1
 lastPow  = 1 * init
 
 count = 1
-while True:
-
-    term = lastPow / lastFact
+while abs(term := lastPow / lastFact) > epsilon:
     aprox += term * (-1, 1)[count % 2]
-
-    if abs(term) < epsilon: break
 
     lastPow  *= init * init
     lastFact *= (count * 2) * (count * 2 + 1)
