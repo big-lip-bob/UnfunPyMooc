a68b932a0aaeb5b367bf6a1df1079ebc47b434e6
big-lip-bob <41026257+big-lip-bob@users.noreply.github.com>
2022-10-09 00:30:34 +0200

    Changes

--- 4/3/soleil_leve.py
+++ 4/3/soleil_leve.py
@@ -1,5 +1,4 @@
 def soleil_leve(lever, coucher, heure):
-    if lever == coucher == 0: return True
-    if lever == coucher == 12: return False
-    if lever <= coucher: return lever <= heure < coucher
+    if lever == coucher: return lever == coucher == 0 # if lever == coucher == 12: return False
+    if lever <  coucher: return lever <= heure < coucher
     return lever <= heure or heure < coucher
