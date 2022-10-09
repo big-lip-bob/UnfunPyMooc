ad02db8743c627ddc7f40dcc6eb5b5f42457d1e6
big-lip-bob <41026257+big-lip-bob@users.noreply.github.com>
2022-10-09 22:09:27 +0200

    Ayy

--- 4/5/pierrepapierciseaux.py
+++ 4/5/pierrepapierciseaux.py
@@ -2,37 +2,16 @@ from random import seed, randint
 
 def bat(j1, j2): # 0 : PIERRE, 1 : FEUILLE, 2 : CISEAUX
     return j1 == (j2 + 2) % 3
-
-def symbol(s):
-    if s == 0: return "Pierre"
-    if s == 1: return "Feuille"
-    if s == 2: return "Ciseaux"
-
-def status(s):
-    if s ==  1: return "est battu par"
-    if s ==  0: return "annule"
-    if s == -1: return "bat"
-
-def match(j1, j2):
-    if bat(j1, j2):
-        return 1
-    elif j1 == j2:
-        return 0
-    else:
-        return -1
+def symbol(s): return ("Pierre", "Feuille", "Ciseaux")[s]
+def status(s): return ("bat", "annule", "est battu par")[s + 1]
+def match(j1, j2): return bat(j1, j2) * 1 or (j1 == j2) - 1
 
 seed(int(input()))
 points = 0
 for i in range(0, 5):
-    j1 = randint(0, 2)
-    j2 = int(input())
+    [j1, j2] = [randint(0, 2), int(input())]
     point = match(j1, j2)
     points += point
     print(f"{symbol(j1)} {status(point)} {symbol(j2)} : {points}")
 
-if 0 < points:
-    print("Gagné")
-elif points == 0:
-    print("Nul")
-else:
-    print("Perdu")
+print("Gagné" if points > 0 else "Nul" if not points else "Perdu")
--- 4/5/rendre_monnaie.py
+++ 4/5/rendre_monnaie.py
@@ -1,13 +1,13 @@
 currency = [20, 10, 5, 2, 1]
-def rendre_monnaie(money, *pennies):
-    if len(pennies) != 5: raise ValueError
+def rendre_monnaie(money, x20, x10, x5, x2, x1): # *pennies): # triste de ne pas pouvoir *pennies
+    pennies = [x20, x10, x5, x2, x1]
     change = sum(x * y for x, y in zip(currency, pennies)) - money
-    if change < 0: return (None, None, None, None, None) # pourquoi pas un seul bof
-
+    if change < 0: return (None, None, None, None, None) # pourquoi pas un seul, bof
     charge = []
     for amount in currency:
         count = change // amount
         charge.append(count)
         change -= count * amount
+        # assert 0 <= change
 
     return tuple(charge)
