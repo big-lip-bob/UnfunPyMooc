abd25d46176d190b1829b31c0fcb4c4935a073d8
big-lip-bob <41026257+big-lip-bob@users.noreply.github.com>
2022-10-09 22:48:56 +0200

    Changes

--- 4/5/rac_eq_2nd_deg.py
+++ 4/5/rac_eq_2nd_deg.py
@@ -1,12 +1,7 @@
-import math
-
-
 def rac_eq_2nd_deg(a, b, c):
     delta = b * b - 4 * a * c
     if 0 < delta:
-        delta = math.sqrt(delta) / (2 * a)
-        r1, r2 = -b / (2 * a) - delta, -b / (2 * a) + delta
-        return (min(r1, r2), max(r1, r2))
-    elif delta == 0:
-        return (-b / (2 * a),)
-    else: return tuple()
+        delta = delta ** .5 / (2 * a)
+        [r1, r2] = [-b / (2 * a) - delta * sign for sign in [-1, 1]]
+        return (r1, r2) if r1 < r2 else (r2,  r1)
+    return delta == 0 and (-b / (2 * a),) or tuple()
