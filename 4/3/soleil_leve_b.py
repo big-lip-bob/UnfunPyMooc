d875c25217b09ab74c17f332c07f53a9560473fc
big-lip-bob <41026257+big-lip-bob@users.noreply.github.com>
2022-10-11 19:13:51 +0200

    Changes

--- 4/3/soleil_leve_b.py
+++ 4/3/soleil_leve_b.py
@@ -4,10 +4,7 @@ def soleil_leve(lever, coucher, heure):
     if lever <= coucher: return lever <= heure < coucher
     return heure < coucher or lever <= heure
 
-a = int(input())
-b = int(input())
-c = int(input())
-d = int(input())
+[a, b, c, d] = [int(input()) for _ in range(4)]
 
 for i in range(0, 24):
     print(f"{i}{'' if soleil_leve(a, b, i) or soleil_leve(c, d, i) else ' *'}")
