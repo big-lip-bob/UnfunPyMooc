80a650f889aff7e6bb634cb30b92f82e12d88365
big-lip-bob <41026257+big-lip-bob@users.noreply.github.com>
2022-10-16 23:17:22 +0200

    Walrus hell yeahh

--- 5/1/longueur.py
+++ 5/1/longueur.py
@@ -1,11 +1,7 @@
 def longueur(*p): # a quand l'iterateur sliding window ?
     if len(p) < 2: return 0
     origin = p[0]
-    acc = 0
-    for p in p[1:]:
-        acc += distance_points(origin, p)
-        origin = p
-    return acc
+    return sum(distance_points(origin, (origin := p)) for p in p[1:])
 
 def distance_points(a, b):
     dx = b[0] - a[0]
