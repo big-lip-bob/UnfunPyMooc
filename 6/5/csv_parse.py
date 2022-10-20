00db4b27e590b5997a248ee8139b934388a03966
big-lip-bob <41026257+big-lip-bob@users.noreply.github.com>
2022-10-20 20:26:24 +0200

    Fixes

--- 6/5/csv_parse.py
+++ 6/5/csv_parse.py
@@ -1,11 +1,13 @@
+def parse_csv(iter, separator = ';'):
+    headers = list(next(iter)[:-1].split(separator))
+    return [line[:-1].split(separator) for line in iter], headers
+
 def open_csv(file, separator = ';'):
     with open(file, 'r', encoding = "UTF-8") as file:
-        headers = list(next(file)[:-1].split(separator))
-        return [line[:-1].split(separator) for line in file], headers
+        return parse_csv(file, separator)
 
-def greater(l, max):
-    for i in l:
-        if i and max < int(i): return True
+def greater(l, max): return any(i and max < int(i) for i in l)
+
 
 [(fails, fnames), (counts, cnames)] = [open_csv(input()) for _ in range(2)] # [open_csv(["result-pass-fail-0.csv", "result-count-0.csv"][_]) for _ in range(2)] #
 
@@ -17,8 +19,7 @@ ftocm = [ftocm[cnames[i]] for i in range(len(cnames))]
 
 max_fails = 10
 for c in range(len(counts)):
-    count = counts[c]
-    fail = fails[c]
+    count, fail = counts[c], fails[c]
     if not greater(count, max_fails):
         for f in range(len(cnames)):
             if fail[ftocm[f]] == "VRAI": points[f] += 1
