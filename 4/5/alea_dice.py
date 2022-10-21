4ef272e063e8e67cb1d8644c64d8e71bb9657b55
big-lip-bob <41026257+big-lip-bob@users.noreply.github.com>
2022-10-21 14:18:34 +0200

    Fixes

--- 4/5/alea_dice.py
+++ 4/5/alea_dice.py
@@ -1,2 +1,2 @@
 from random import randint, seed
-def alea_dice(rnd): return seed(rnd) or {randint(1, 6) for i in range(0, 3)} == {1, 2, 4}
+def alea_dice(rnd): return seed(rnd) or {randint(1, 6) for _ in range(0, 3)} == {1, 2, 4}
