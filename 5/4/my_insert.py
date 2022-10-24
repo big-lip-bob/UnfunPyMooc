fc6b863de33907a5931f161949fa2207431e9d5c
RustHaruspex <41026257+big-lip-bob@users.noreply.github.com>
2022-10-24 16:02:26 +0200

    Update my_insert.py

--- 5/4/my_insert.py
+++ 5/4/my_insert.py
@@ -1,17 +1,11 @@
 def my_insert(l, v):
     if type(v) != int: return None
-    i = bin(l, v)
-    l = l[:]
-    l[i:i] = [v]
-    return l
-
-def bin(s, v):
-    l = 0
-    h = len(s)
-    while l < h:
-        m = (l + h) // 2
-        if   s[m] == v: return m
-        elif s[m] <  v: l = m + 1
-        else          : h = m - 1
-    return l # == h
-
+    n = []
+    for i in l:
+        if i < v: n += [i]
+        else:
+            n += [v, i]
+            break
+    else: n += [v]
+    for i in l[len(n)-1:]: n += [i]
+    return n
