93188375c248ce41945ac2a09da029f3666a176c
RustHaruspex <41026257+big-lip-bob@users.noreply.github.com>
2022-10-25 18:34:08 +0200

    Update liste_des_mots.py

--- 5/8/liste_des_mots.py
+++ 5/8/liste_des_mots.py
@@ -1,7 +1,5 @@
 import re # regex yikes
 
 def liste_des_mots(txt):
-    l = set()
-    with open(txt, encoding="UTF8") as inp:
-        for line in inp: l |= set([c.lower() for c in re.split("[\\d\\s\\-\"'?!:;.,*=()]+", line)])
-    return sorted(l - {''})
+    with open(txt, encoding="UTF8") as file:
+        return sorted({word.lower() for word in re.split("[\\d\\s\\-\"'?!:;.,*=()]+", line) for line in file } - {''})
