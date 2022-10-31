761a7918b84067c30971afe4d83dff7b24a7f7d8
big-lip-bob <41026257+big-lip-bob@users.noreply.github.com>
2022-10-31 15:49:23 +0100

    Broken "Test" Solutions

--- I/1/approx_arccos.py
+++ I/1/approx_arccos.py
@@ -5,7 +5,7 @@ eps, nmax = 1.0e-5, 50
 def approx_arccos(x):
     term = 1
     acc = pi/2 - x
-    for j in range(1, nmax-1):
+    for j in range(1, nmax):
         term *= (2*j-1) / (2*j)
         if abs(cn := term * (x ** (2*j+1)) / (2*j+1)) < eps: break
         acc += cn
