f06e16f51dc3aa256f410586f84ac86fca3dc637
RustHaruspex <41026257+big-lip-bob@users.noreply.github.com>
2022-11-05 17:49:06 +0100

    Update top_3_candidats.py

--- 6/1/top_3_candidats.py
+++ 6/1/top_3_candidats.py
@@ -1,10 +1,8 @@
 def top_3_candidats(moyennes):
-    max = [(None, 0)] * 3
+    max = [(None, 0)] * 4
     for candidat in moyennes:
-        points = moyennes[candidat]
-        if max[-1][1] < points:
-            max[-1] = (candidat, points)
-            for i in reversed(range(len(max)-1)):
-                if max[i][1] < max[i+1][1]: max[i], max[i+1] = max[i+1], max[i]
+        max[-1] = (candidat, moyennes[candidat])
+        for i in reversed(range(len(max)-1)):
+            if max[i][1] < max[i+1][1]: max[i], max[i+1] = max[i+1], max[i]
 
-    return tuple([candidat[0] for candidat in max])
+    return tuple([candidat[0] for candidat[:-1] in max])
